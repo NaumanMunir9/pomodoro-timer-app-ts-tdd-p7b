722b175635327e718955195b031105958e867eda
Nauman Munir <naumanmunir@gmail.com>
2021-03-12 18:21:21 +0500

    added the app component with the corresponding test suite and the css styles sheet

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <h1>Hi from App</h1>
+    </div>
+  );
+};
+
+export default App;
